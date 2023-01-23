cca12317f9fbbc3aeade163ae6a7c652002589dc
callumrowston <callum.rowsto@gmail.com>
2023-01-23 17:43:46 +1100

    Added member get routes

--- routes/member_routes.js
+++ routes/member_routes.js
@@ -3,8 +3,23 @@ import { MemberModel } from '../models/member.js'
 
 const memberRoutes = express.Router()
 
+// Get all members
 memberRoutes.get('/', async (req, res) => res.send(await MemberModel.find().select('username email joined_date')))
 
-memberRoutes.get('/:id', async (req, res) => res.send(await MemberModel.find().select('username email joined_date')))
+// Get single member by id
+memberRoutes.get('/:id', async (req, res) => {
+    try {
+        const member = await MemberModel.findById(req.params.id).select('username email joined_date')
+        if (member) {
+            res.send(member)
+        } 
+        else {
+            res.status(404).send({ error: 'Member not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
 
 export default memberRoutes
