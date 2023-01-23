1f3eecd39ff736205e37723b97dea3d61ba3841e
callumrowston <callum.rowsto@gmail.com>
2023-01-23 16:28:11 +1100

    Created GET post by id route

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -3,9 +3,24 @@ import { PostModel } from '../models/post.js'
 
 const postRoutes = express.Router()
 
+// Get all posts
 postRoutes.get('/', async (req, res) => res.send(await PostModel.find().populate({path: 'author', select: 'username'})))
 
-
+// Get single post by id
+postRoutes.get('/:id', async (req, res) => {
+    try {
+        const post = await PostModel.findById(req.params.id).populate({ path: 'author', select: 'username' })
+            if (post) {
+                res.send(post)
+            } 
+            else {
+                res.status(404).send({ error: 'Post not found' })
+            }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
 
 
 export default postRoutes
