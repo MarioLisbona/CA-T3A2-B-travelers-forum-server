dcd6faa6123eeb8f0025a03ada80ac7a34fc03a0
callumrowston <callum.rowsto@gmail.com>
2023-01-25 11:59:23 +1100

    Adjusted register and login jwt tokenisation

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -3,17 +3,20 @@ import { MemberModel } from '../models/member.js'
 import { body, validationResult } from 'express-validator'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const authRoutes = express.Router()
 
 // Create JWT
 // expireIn: Days, hours, minutes, seconds
-const tokenAge = 1 * 24 * 60 * 60;
-const createToken = (id) => {
-    return jwt.sign({ id }, 'secret', {
-        expiresIn: tokenAge
-    })
-}
+// const tokenAge = 1 * 24 * 60 * 60;
+// const createToken = (id) => {
+//     return jwt.sign({ id }, 'secret', {
+//         expiresIn: tokenAge
+//     })
+// }
 
 // Register User
 authRoutes.post(
@@ -37,16 +40,29 @@ authRoutes.post(
                 email,
                 password
             })
-            const token = createToken(newMember._id)
-            res.cookie('jwt', token, { httpOnly: true, tokenAge: tokenAge*1000} )
-            res.status(201).json({ newMember: newMember._id })
+            const token = jwt.sign(
+                { memberId: newMember._id, username: newMember.username},
+                process.env.JWT_SECRET,
+                { expiresIn: "24h"}
+            )
+            // const token = createToken(newMember._id)
+            // res.cookie('jwt', token, { httpOnly: true, tokenAge: tokenAge*1000} )
+            // res.status(201).json({ newMember: newMember._id })
+            res.status(201).json({
+                success: true,
+                data: {
+                    memberId: newMember._id, 
+                    username: newMember.username,
+                    token: token
+                }
+            })
         }
         catch (err) {
             res.status(500).send({ error: err.message })
         }
 })
 
-const jwtVerify = async (username, password) => {
+const jwtValidate = async (username, password) => {
     const member = await MemberModel.findOne({ username })
     if (member) {
         const auth = await bcrypt.compare(password, member.password)
@@ -65,10 +81,22 @@ authRoutes.post(
     async (req, res) => {
         const { username, password } = req.body
         try {
-            const loginMember = await jwtVerify(username, password)
-            const token = createToken(loginMember._id)
-            res.cookie('jwt', token, { httpOnly: true, tokenAge: tokenAge*1000 })
-            res.status(201).json({ member: loginMember._id, token: token });
+            const loginMember = await jwtValidate(username, password)
+            const token = jwt.sign(
+                { memberId: loginMember._id, username: loginMember.username},
+                process.env.JWT_SECRET,
+                { expiresIn: "24h"}
+            )
+            res.status(201).json({
+                success: true,
+                data: {
+                    memberId: loginMember._id, 
+                    username: loginMember.username,
+                    token: token
+                }
+            })
+            // res.cookie('jwt', token, { httpOnly: true, tokenAge: tokenAge*1000 })
+            // res.status(201).json({ member: loginMember._id, token: token });
         }
         catch (err) {
             res.status(500).send({ error: err.message })
