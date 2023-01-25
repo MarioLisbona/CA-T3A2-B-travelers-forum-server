4bb0ca3d28c3a8bda8b078c30c33bf95514ebde0
callumrowston <callum.rowsto@gmail.com>
2023-01-25 17:23:55 +1100

    Added validate token function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import postRoutes from './routes/post_routes.js'
 import memberRoutes from './routes/member_routes.js'
-import authRoutes from './routes/auth_routes.js'
+import { authRoutes } from './routes/auth_routes.js'
 import { MemberModel } from './models/member.js'
 import { dbConnect } from './db.js'
 // Import models
