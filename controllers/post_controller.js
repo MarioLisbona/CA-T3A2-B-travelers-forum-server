81b9f1fb42ab6c68f61b688b70028b5185136662
callumrowston <callum.rowsto@gmail.com>
2023-01-26 17:19:29 +1100

    Abstracted post  routes logic to controllers

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -32,15 +32,15 @@ const getPost = async (req, res) => {
 
 const getCategory = async (req, res) => {
     try {
-        const postCategory = await PostModel
+        const posts = await PostModel
             .find({ category: req.params.category })
             .sort({ date_posted: 'desc' })
             .populate({ path: 'author', select: 'username' })
             .populate({ path: 'comments', populate: {
                 path: 'author', select: 'username'
             }})
-        if (postCategory) {
-            res.status(201).send(postCategory)
+        if (posts) {
+            res.status(201).send(posts)
         } 
     } catch (err) {
         res.status(500).send({ error: err.message })
@@ -48,14 +48,50 @@ const getCategory = async (req, res) => {
 }
 
 const createPost = async (req, res) => {
-    const { title, author, category, content  } = req.body
-    const insertPost = await PostModel.create({ 
-        title, 
-        author: req.id || author, 
-        category, 
-        content
-    })
-    res.status(201).send(await insertPost.populate({ path: 'author', select: 'username' }))
+    try {
+        const { title, author, category, content  } = req.body
+        const insertPost = await PostModel.create({ 
+            title, 
+            author: req.id || author, 
+            category, 
+            content
+        })
+        if (insertPost) {
+            res.status(201).send(await insertPost
+                .populate({ path: 'author', select: 'username' }))
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
 }
 
-export { getAllPosts, getPost, getCategory, createPost }
+const updatePost = async (req, res) => {
+    try {
+        const { title, category, content  } = req.body
+        const updatedPost = { title, category, content }
+        const post = await PostModel
+        .findByIdAndUpdate(req.params.id, updatedPost, 
+            { returnDocument: 'after' })
+            
+        if (post) {
+            res.status(201).send(await post
+                .populate({ path: 'author', select: 'username' }))
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const post = await PostModel.findByIdAndDelete(req.params.id)
+        if (post) {
+            res.sendStatus(204)
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+export { getAllPosts, getPost, getCategory, createPost, updatePost, deletePost }
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -1,20 +1,16 @@
 import express from 'express'
-import { PostModel } from '../models/post.js'
-import { CommentModel } from '../models/comment.js'
-import { param, body, validationResult } from 'express-validator'
-import { validateToken } from './auth_routes.js'
 import { validatePost, validateRequestSchema } from '../middleware/auth.js'
-import { getAllPosts, getPost, getCategory, createPost } from '../controllers/post_controller.js'
+import { getAllPosts, getPost, getCategory, createPost, updatePost, deletePost } from '../controllers/post_controller.js'
 
 const postRoutes = express.Router()
 
 // Get all posts
 postRoutes.get('/', getAllPosts)
 
-// Get single post by id and its comments
+// Get single post by id
 postRoutes.get('/:id', getPost)
 
-// Get all posts by category
+// Get all posts in a category
 postRoutes.get('/category/:category', getCategory)
 
 // Post new post
@@ -23,36 +19,10 @@ postRoutes.post('/new', validatePost, validateRequestSchema, createPost)
 
 // Update post
 // JWT
-postRoutes.put('/:id', async (req, res) => {
-    const { title, category, content  } = req.body
-    const updatedPost = { title, category, content }
-    try {
-        const post = await PostModel.findByIdAndUpdate(req.params.id, updatedPost, { returnDocument: 'after' })
-        if (post) {
-            res.send(post)
-        } else {
-            res.status(404).send({ error: 'Post not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
+postRoutes.put('/:id', updatePost)
 
 // Delete post
 // JWT
-postRoutes.delete('/:id', async (req, res) => {
-    try {
-        const post = await PostModel.findByIdAndDelete(req.params.id)
-        if (post) {
-            res.sendStatus(204)
-        } else {
-            res.status(404).send({ error: 'Post not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
+postRoutes.delete('/:id', deletePost)
 
 export default postRoutes
