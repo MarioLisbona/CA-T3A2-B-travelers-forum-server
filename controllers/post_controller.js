6e63d0e40fa36e93f216924b90ea82b19b9202e5
callumrowston <callum.rowsto@gmail.com>
2023-01-26 15:57:28 +1100

    abstracted get single post, get category

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,52 @@
 import { PostModel } from "../models/post.js"
 
+const getAllPosts = async (req, res) => {
+    try {
+        const allPosts = await PostModel
+            .find().sort({ date_posted: 'desc' })
+            .populate({ path: 'author', select: 'username' })
+            .populate({ path: 'comments', populate: {
+                path: 'author', select: 'username'
+            }})
+        res.status(201).send(allPosts)
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const getPost = async (req, res) => {
+    try {
+        const post = await PostModel
+            .findById(req.params.id)
+            .populate({ path: 'author', select: 'username' })
+            .populate({ path: 'comments', populate: {
+                path: 'author', select: 'username'
+            }})
+        if (post) {
+            res.status(201).send(post)
+        } 
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const getCategory = async (req, res) => {
+    try {
+        const postCategory = await PostModel
+            .find({ category: req.params.category })
+            .sort({ date_posted: 'desc' })
+            .populate({ path: 'author', select: 'username' })
+            .populate({ path: 'comments', populate: {
+                path: 'author', select: 'username'
+            }})
+        if (postCategory) {
+            res.status(201).send(postCategory)
+        } 
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
 const createPost = async (req, res) => {
     const { title, author, category, content  } = req.body
     const insertPost = await PostModel.create({ 
@@ -11,4 +58,4 @@ const createPost = async (req, res) => {
     res.status(201).send(await insertPost.populate({ path: 'author', select: 'username' }))
 }
 
-export { createPost }
+export { getAllPosts, getPost, getCategory, createPost }
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -4,80 +4,22 @@ import { CommentModel } from '../models/comment.js'
 import { param, body, validationResult } from 'express-validator'
 import { validateToken } from './auth_routes.js'
 import { validatePost, validateRequestSchema } from '../middleware/auth.js'
-import { createPost } from '../controllers/post_controller.js'
+import { getAllPosts, getPost, getCategory, createPost } from '../controllers/post_controller.js'
 
 const postRoutes = express.Router()
 
-const categories = ['Asia', 'Africa', 'North America', 'South America', 'Antarctica', 'Europe', 'Australia']
-
 // Get all posts
-postRoutes.get('/', async (req, res) => res.send(
-    await PostModel.find().sort({ date_posted: 'desc' })
-    // Populate author and comments nested fields
-    .populate({ path: 'author', select: 'username' })
-    .populate({ path: 'comments', populate: {
-        path: 'author', select: 'username'
-    }})
-))
+postRoutes.get('/', getAllPosts)
 
 // Get single post by id and its comments
-postRoutes.get('/:id', async (req, res) => {
-    try {
-        const post = await PostModel.findById(req.params.id)
-        .populate({ path: 'author', select: 'username' })
-        .populate({ path: 'comments', populate: {
-        path: 'author', select: 'username'
-        }})
-        if (post) {
-            res.send(post)
-        } 
-        else {
-            res.status(404).send({ error: `Post not found with id: ${req.params.id}` })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
+postRoutes.get('/:id', getPost)
 
 // Get all posts by category
-postRoutes.get('/category/:category', param('category').isIn(categories), async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).send({ error: 'Not a valid category'})
-    }
-    try {
-        const post = await PostModel.find({ category: req.params.category }).sort({ date_posted: 'desc' }).populate({ path: 'author', select: 'username' })
-            if (post) {
-                res.send(post)
-            } 
-            else {
-                res.status(404).send({ error: 'No posts found in that category' })
-            }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
+postRoutes.get('/category/:category', getCategory)
 
 // Post new post
 // JWT
-postRoutes.post(
-    '/new',
-    validatePost,
-    validateRequestSchema,
-    createPost
-    // async (req, res) => {
-    //     const { title, author, category, content  } = req.body
-    //     const insertPost = await PostModel.create({ 
-    //         title, 
-    //         author: req.id || author, 
-    //         category, 
-    //         content
-    //     })
-    //     res.status(201).send(await insertPost.populate({ path: 'author', select: 'username' }))
-    // }
-)
+postRoutes.post('/new', validatePost, validateRequestSchema, createPost)
 
 // Update post
 // JWT
