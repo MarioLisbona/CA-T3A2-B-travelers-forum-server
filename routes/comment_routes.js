ab0ae5f71aae92ea743e549078f42bc0a088b736
callumrowston <callum.rowsto@gmail.com>
2023-01-27 14:29:16 +1100

    removed jwt requirement for post and comment for now

--- routes/comment_routes.js
+++ routes/comment_routes.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { createComment } from '../controllers/comment_controller.js'
-import { validateComment, validateToken, validateRequestSchema } from '../middleware/auth.js'
+import { validateComment, validateRequestSchema } from '../middleware/auth.js'
 
 const commentRoutes = express.Router()
 
 // Create comment
 // Still need to add JWT
 commentRoutes.post('/new',
-    validateToken,
     validateComment,
     validateRequestSchema,
     createComment
