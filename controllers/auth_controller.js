4fc332065a9358b76f00fa71b9189ff2c3479ab3
callumrowston <callum.rowsto@gmail.com>
2023-01-30 17:29:34 +1100

    changed jwt expiry to 5 mins

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -70,7 +70,7 @@ const createToken = (member) => {
     const accessToken = jwt.sign({
         id: member._id
     }, process.env.JWT_SECRET, 
-    {expiresIn: '1d'})
+    {expiresIn: '5m'})
     return accessToken
 }
 
