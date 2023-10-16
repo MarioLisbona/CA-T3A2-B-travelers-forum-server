bdfde14b596ea351eb067620b64cb4c69bdf5c95
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-10-16 22:15:33 +1100

    update seed.js

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-import express from 'express'
-import cors from 'cors'
-import postRoutes from './routes/post_routes.js'
-import memberRoutes from './routes/member_routes.js'
-import authRoutes from './routes/auth_routes.js'
-import commentRoutes from './routes/comment_routes.js'
-import { dbConnect } from './db.js'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import postRoutes from "./routes/post_routes.js";
+import memberRoutes from "./routes/member_routes.js";
+import authRoutes from "./routes/auth_routes.js";
+import commentRoutes from "./routes/comment_routes.js";
+import { dbConnect } from "./db.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config()
+dotenv.config();
 
 // If statement to connect to different DB for testing
-if (process.env.NODE_ENV === 'test') {
-    try {
-        await mongoose.connect(process.env.MONGO_URI_TEST)
-    }
-    catch (error) {
-        console.error(error)
-    }
+if (process.env.NODE_ENV === "test") {
+  try {
+    await mongoose.connect(process.env.MONGO_URI_TEST);
+  } catch (error) {
+    console.error(error);
+  }
 } else {
-    dbConnect()
+  dbConnect();
 }
 
+const app = express();
 
-const app = express()
+app.use(cors());
 
-app.use(cors())
-
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use('/posts', postRoutes)
-app.use('/members', memberRoutes)
-app.use('/auth', authRoutes)
-app.use('/comments', commentRoutes)
+app.use("/posts", postRoutes);
+app.use("/members", memberRoutes);
+app.use("/auth", authRoutes);
+app.use("/comments", commentRoutes);
 
 // Home Test Route
-app.get('/', (req, res) => res.status(200).send({ test_response: 'Test GET Request successful' }))
+app.get("/", (req, res) =>
+  res.status(200).send({ test_response: "Test GET Request successful" })
+);
 
-export default app
+export default app;
